391def5c5bc6159c6758d433bc50dc7507d6a6e2
kotovvv <kotovvv@gmail.com>
2022-09-15 11:28:55 +0300

    not set user when clear

--- resources/js/components/crmanager/lidsplus.vue
+++ resources/js/components/crmanager/lidsplus.vue
@@ -615,7 +615,7 @@ export default {
   },
   methods: {
     clearuser(){
-       this.disableuser = this.selectedUser.id || 0;
+       this.disableuser = 0;
        this.getLidsOnUserOrDate()
        this.selectedUser = {};
     },
