3a747d8653ec1287ef63cdc9a5962fb116255752
kotovvv <kotovvv@gmail.com>
2023-02-02 10:43:49 +0200

    fix

--- app/Http/Controllers/API/v1/ImportsController.php
+++ app/Http/Controllers/API/v1/ImportsController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use App\Models\Import;
 use DB;
 use Debugbar;
-use App\Models\Depozit;
 
 class ImportsController extends Controller
 {
@@ -63,16 +62,16 @@ class ImportsController extends Controller
     $dateTo = $req['dateto'];
     $onlynew = $req['onlynew'];
 
-    $sql = "SELECT bl.`id`, bl.`address`, bl.`summ`,bl.`office_id`, bl.`other`, bl.`trx_count`, l.`id` lid_id ,l.`name`,l.`tel`,l.`email`, l.`provider_id`, l.`status_id`, s.`name` s_name, s.`color` s_color,p.`name` p_name FROM `btc_list` bl INNER JOIN `lids` l ON (bl.`lid_id` = l.`id` ) INNER JOIN `providers` p ON (l.`provider_id` = p.`id` ) INNER JOIN `statuses` s ON (l.`status_id` = s.`id` )  WHERE " . $where . " `other` REGEXP '[^|].*' ORDER BY lid_id ASC";
+    $sql = "SELECT bl.`id`, bl.`address`, bl.`summ`, bl.`office_id`, bl.`other`, bl.`trx_count`, l.`id` lid_id, l.`name`, l.`tel`, l.`email`, l.`provider_id`, l.`status_id`, s.`name` s_name, s.`color` s_color, p.`name` p_name, (SELECT IF (SUM(d.`depozit`), SUM(d.`depozit`), '') FROM `depozits` d WHERE l.`id` = d.`lid_id` AND d.`created_at` > '" . $dateFrom . " 00:00:00' AND d.`created_at` < '" . $dateTo . " 23:59:59') depozit FROM `btc_list` bl INNER JOIN `lids` l ON (bl.`lid_id` = l.`id`) INNER JOIN `providers` p ON (l.`provider_id` = p.`id`) INNER JOIN `statuses` s ON (l.`status_id` = s.`id`) WHERE " . $where . "`other` REGEXP '[^|].*' ORDER BY l.`id`";
     $rows = DB::select(DB::raw($sql));
-
     //array dates (from to)
     $a_list_date = $this->date_range($dateFrom, $dateTo);
-    $res['data'] = [];
+    $res['data'] = $data =  [];
     $res['providers'] = [];
     $res['statuses'] = [];
     $res['result'] = "success";
-$lid_id = 0;
+
+    $compareLidId = $sum_lid = $ia =  0;
     if ($rows) {
       //foreach row
       foreach ($rows as $lid) {
@@ -94,18 +93,21 @@ $lid_id = 0;
           continue;
         }
         if ($a_intersect) {
-          $depozit = Depozit::select(DB::Raw('sum(`depozit`) as depozit'))->whereBetween('created_at', [$dateFrom . " 00:00:00", $dateTo . " 23:59:59"])->where('lid_id', $lid->lid_id)->groupBy('lid_id')->value('depozit');
           foreach ($a_intersect as $key => $date) {
             if ($this->between_dates($date, $dateFrom, $dateTo)) {
               $sum_dat += $a_date_sum[0][($key + 1) * 2];
             }
+            $res['providers'][] = ['id' => $lid->provider_id, 'name' => $lid->p_name];
+            $res['statuses'][] = ['id' => $lid->status_id, 'name' => $lid->s_name, 'color' => $lid->s_color];
           }
-          $res['providers'][] = ['id' => $lid->provider_id, 'name' => $lid->p_name];
-          $res['statuses'][] = ['id' => $lid->status_id, 'name' => $lid->s_name, 'color' => $lid->s_color];
-          //add sum_dat to row
-          if ($lid_id != $lid->lid_id) {
-            $lid_id = $lid->lid_id;
-            $res['data'][$lid->lid_id] = [
+          // group same lids
+          if ($compareLidId == $lid->lid_id) {
+            $data[$ia - 1]['summ']  += $lid->summ;
+            $data[$ia - 1]['sum_dat']  += $sum_dat;
+            continue;
+          } else {
+            $compareLidId = $lid->lid_id;
+            $data[$ia++] = [
               'id' => $lid->id,
               'name' => $lid->name,
               'email' => $lid->email,
@@ -119,17 +121,15 @@ $lid_id = 0;
               'provider_id' => $lid->provider_id,
               'p_name' => $lid->p_name,
               'sum_dat' => $sum_dat,
-              'depozit' => $depozit
+              'depozit' => (int) $lid->depozit
             ];
-          } else {
-            $res['data'][$lid->lid_id]['summ'] += $lid->summ;
-            $res['data'][$lid->lid_id]['sum_dat'] += $sum_dat;
           }
         }
         //next row
       }
     }
-    $res['data'] = array_values($res['data']);
+
+    $res['data'] = array_values($data);
     return $res;
   }
 
