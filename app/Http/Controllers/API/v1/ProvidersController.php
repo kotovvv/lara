5962022c03dfb92c32e4ce01ee4da01864b5052f
kotovvv <kotovvv@gmail.com>
2023-02-08 19:16:05 +0200

    fix report provider depozit for time

--- app/Http/Controllers/API/v1/ProvidersController.php
+++ app/Http/Controllers/API/v1/ProvidersController.php
@@ -207,7 +207,7 @@ class ProvidersController extends Controller
   {
     $sql = "SELECT `status_id`,s.`name`,s.`color`,COUNT(`status_id`) hm FROM `lids` l LEFT JOIN `statuses` s ON (s.`id` = l.`status_id`) WHERE l.status_id != 10 AND `provider_id` = " . (int) $id . " AND CAST(l.`created_at` AS DATE) BETWEEN '" . $start_day . "' AND '" . $stop_day . "' GROUP BY `status_id` ORDER BY s.order ASC";
     $providerTimeLidsNoDep = DB::select(DB::raw($sql));
-    $sql = "SELECT `status_id`,s.`name`,s.`color`,COUNT(`status_id`) hm FROM `lids` l LEFT JOIN `statuses` s ON (s.`id` = l.`status_id`) WHERE l.status_id = 10 AND `provider_id` = " . (int) $id . " AND CAST(l.`created_at` AS DATE) BETWEEN '" . $start_day . "' AND '" . $stop_day . "' GROUP BY `status_id` ORDER BY s.order ASC";
+    $sql = "SELECT `status_id`,s.`name`,s.`color`,COUNT(`status_id`) hm FROM `lids` l RIGHT JOIN `depozits` d ON (d.`lid_id` = l.`id`) LEFT JOIN `statuses` s ON (s.`id` = l.`status_id`) WHERE l.status_id = 10 AND `provider_id` = " . (int) $id . " AND CAST(l.`created_at` AS DATE) BETWEEN '" . $start_day . "' AND '" . $stop_day . "' GROUP BY `status_id` ORDER BY s.order ASC";
     $providerTimeLidsDep = DB::select(DB::raw($sql));
 
     $providerTimeLids = array_merge($providerTimeLidsNoDep, $providerTimeLidsDep);
@@ -233,7 +233,7 @@ class ProvidersController extends Controller
   // Get lids for time provider
   public function getDataTime($id, $start_day, $stop_day)
   {
-    $sql = "SELECT l.id, l.`name`, l.`tel`, l.`email`, cast(l.`created_at` as date) created_at, cast(l.`updated_at` as date) updated_at, l.`status_id`, s.`name` status_name, s.`color` FROM `lids` l LEFT JOIN `statuses` s ON (s.`id` = l.`status_id`) WHERE `provider_id` = " . (int) $id . " AND CAST(l.`created_at` AS DATE) BETWEEN '" . $start_day . "' AND '" . $stop_day . "'  ORDER BY s.order ASC";
+    $sql = "SELECT l.id, l.`name`, l.`tel`, l.`email`, cast(l.`created_at` as date) created_at, cast(l.`updated_at` as date) updated_at, l.`status_id`, s.`name` status_name, s.`color` FROM `lids` l RIGHT JOIN `depozits` d ON (d.`lid_id` = l.`id`) LEFT JOIN `statuses` s ON (s.`id` = l.`status_id`) WHERE `provider_id` = " . (int) $id . " AND CAST(l.`created_at` AS DATE) BETWEEN '" . $start_day . "' AND '" . $stop_day . "'  ORDER BY s.order ASC";
     $providerTimeLids = DB::select(DB::raw($sql));
 
     return response()->json([
