b968ca2f8cdda90825e2a6c941d754a9dcecdeaa
kotovvv <kotovvv@gmail.com>
2023-12-22 18:12:00 +0200

    insert import date fix

--- resources/js/components/admin/importcsv.vue
+++ resources/js/components/admin/importcsv.vue
@@ -485,8 +485,12 @@ export default {
             //======================
             let info = {};
 
-            info.start = response.data.date_start;
-            info.end = response.data.date_end;
+            info.start = response.data.date_start
+              .substring(0, 19)
+              .replace("T", " ");
+            info.end = response.data.date_end
+              .substring(0, 19)
+              .replace("T", " ");
             info.provider_id = self.selectedProvider;
             info.user_id = self.$attrs.user.id;
             info.message = self.message;
@@ -523,8 +527,12 @@ export default {
             //======================
             let info = {};
 
-            info.start = response.data.date_start;
-            info.end = response.data.date_end;
+            info.start = response.data.date_start
+              .substring(0, 19)
+              .replace("T", " ");
+            info.end = response.data.date_end
+              .substring(0, 19)
+              .replace("T", " ");
             info.provider_id = self.selectedProvider;
             info.user_id = self.$attrs.user.id;
             info.message = self.message;
