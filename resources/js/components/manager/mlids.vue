6672e3532c2d25b12ae9097efd8b8074b46dd94b
kotovvv <kotovvv@gmail.com>
2024-07-18 18:48:49 +0300

    deposit set time

--- resources/js/components/manager/mlids.vue
+++ resources/js/components/manager/mlids.vue
@@ -464,7 +464,9 @@
                 block
                 height="100%"
                 :disabled="
-                  (selectedStatus == 10 && depozit_val < 1) ||
+                  (selectedStatus == 10 &&
+                    depozit_val < 1 &&
+                    $refs.datetime.date == '') ||
                   (selected.length &&
                     selected[0].status_id == 32 &&
                     text_message == '' &&
